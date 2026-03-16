6ca1508e7cea24fc5d850361cad193153b862c49
Daim Khan <daimahmadkhan@gmail.com>
2026-03-16 19:55:53 +0530

    implement login/register functionality

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const authRouter = require("./routes/auth.route");
 const postRouter = require("./routes/post.route");
@@ -9,6 +10,12 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  }),
+);
 
 app.use("/api/auth", authRouter);
 app.use("/api/posts", postRouter);
--- frontend/src/features/auth/pages/Register.jsx
+++ frontend/src/features/auth/pages/Register.jsx
@@ -1,15 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router";
+import axios from "axios";
 
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+   axios
+      .post(
+        "http://localhost:3000/api/auth/register",
+        {
+          username,
+          email,
+          password,
+        },
+        {
+          withCredentials: true,
+        },
+      )
+      .then(res => {
+        console.log(res.data);
+      });
+  }
+
   return (
     <main>
       <div className="form-container">
         <h1>Register</h1>
-        <form>
-          <input type="text" name="username" placeholder="Enter username" />
-          <input type="text" name="email" placeholder="Enter email" />
-          <input type="text" name="password" placeholder="Enter password" />
+        <form onSubmit={handleSubmit}>
+          <input
+            onInput={(e) => {
+              setUsername(e.target.value);
+            }}
+            type="text"
+            name="username"
+            placeholder="Enter username"
+          />
+          <input
+            onInput={(e) => {
+              setEmail(e.target.value);
+            }}
+            type="text"
+            name="email"
+            placeholder="Enter email"
+          />
+          <input
+            onInput={(e) => {
+              setPassword(e.target.value);
+            }}
+            type="text"
+            name="password"
+            placeholder="Enter password"
+          />
           <button type="submit">Register</button>
         </form>
 
